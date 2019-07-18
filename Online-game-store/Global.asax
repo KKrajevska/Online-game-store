be13891345230d92fc22763061b3b532e431db22
Natasha Ilievska <ilievska28@gmail.com>
2019-07-18 18:59:32 +0200

    initial commit

---
+++ Online-game-store/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Online_game_store.MvcApplication" Language="C#" %>
---
+++ Online-game-store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_game_store.Startup))]
+namespace Online_game_store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
